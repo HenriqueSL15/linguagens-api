47ae4c18e8face846a20d4a6571b34cf0ac3e779
HenriqueSL15 <henrynyyls@gmail.com>
2023-04-13 09:45:18 -0300

    Initial commit

---
+++ src/main/java/br/com/alura/linguagensapi/LinguagemRepository.java
@@ -0,0 +1,8 @@
+package br.com.alura.linguagensapi;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface LinguagemRepository extends MongoRepository<Linguagem, String>{
+    
+        
+}
